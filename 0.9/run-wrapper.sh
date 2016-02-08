8e256584ee780d5a953b71a8b1e7a04853d349ec
Jon Seymour <jon@wildducktheories.com>
2016-02-08 18:46:19 +1100

    invoke influxd from path

--- 0.9/run-wrapper.sh
+++ 0.9/run-wrapper.sh
@@ -6,4 +6,4 @@ CONFIG_FILE="/config/config.toml"
 (. ${CONFIG_TEMPLATE}) > ${CONFIG_FILE}
 $(dirname "$0")/run.sh
 
-exec /opt/influxdb/influxd -config=${CONFIG_FILE}
+exec influxd -config=${CONFIG_FILE}
